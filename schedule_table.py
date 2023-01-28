7b9a3c11c77fba161c804c35897526436e7ee580
SakuraPuare <java20131114@gmail.com>
2023-01-28 18:37:27 +0800

    🦄 refactor: 课程表类

--- schedule_table.py
+++ schedule_table.py
@@ -1,4 +1,3 @@
-import io
 import json
 import pathlib
 import pickle
@@ -6,6 +5,14 @@ from typing import Union, Any
 
 import pandas
 
+from request import Request
+
+
+class Schedule:
+
+    def __init__(self, schedule) -> None:
+        if isinstance(schedule, dict):
+            self.schedule = schedule
         elif isinstance(schedule, str):
             self.schedule = self.load_from_file(schedule)
         elif isinstance(schedule, bytes):
@@ -13,11 +20,81 @@ import pandas
         else:
             self.schedule = {}
 
+    def load_from_website(self, request: Request, semester: str) -> dict:
+        assert request.is_login
+        schedule = request.get_schedule(semester)
+        return self.load_schedule(schedule)
+
+    def load_from_file(self, name: str) -> dict:
+        if name.endswith('.xls'):
             with open(name, 'rb') as f:
                 schedule = self.load_schedule(f.read())
+        elif name.endswith('.json'):
+            schedule = self.load_schedule_from_json(name)
+        elif name.endswith('.pickle'):
+            schedule = self.load_schedule_from_pickle(name)
+        else:
+            raise ValueError('不支持的文件类型')
+        return schedule
+
+    @staticmethod
+    def load_schedule(byte: bytes) -> dict:
+        excel = pandas.read_excel(byte, sheet_name='Sheet1')
+        lesson_dict = {}
+        for column in excel.columns[1:]:
+            # 遍历表格每一行
+            day = excel[column][1]
+            lesson_dict[day] = {}
+            for index, row in excel[column].items():
+                if 1 < index < 7:
+                    lesson_list = set()
+                    classes_list = row.split('\n\n')
+                    for classes in classes_list:
+                        if classes == ' ':
+                            lesson = Lesson()
+                        else:
+                            info = [i for i in classes.split('\n') if i != '']
+                            lesson = Lesson(*info)
+                        lesson_list.add(lesson)
+                    lesson_dict[day][index - 2] = list(lesson_list)
+        return lesson_dict
+
+    def load_schedule_from_json(self, name: str = 'schedule.json') -> dict:
+        path = pathlib.Path(name)
+        with open(path, 'r', encoding='u8') as f:
+            return json.load(f, object_hook=self.LessonDecoder)
+
+    @staticmethod
+    def load_schedule_from_pickle(name: str = 'schedule.pickle') -> dict:
+        path = pathlib.Path(name)
+        with open(path, 'rb') as f:
+            return pickle.load(f)
+
+    def save_schedule_as_json(self, name: str = 'schedule.json') -> None:
+        path = pathlib.Path(name)
+        with open(path, 'w', encoding='u8') as f:
+            json.dump(self.schedule, f, indent=4, ensure_ascii=False, cls=Lesson.LessonEncoder)
+
+    def save_schedule_as_pickle(self, name: str = 'schedule.pickle') -> None:
+        path = pathlib.Path(name)
+        with open(path, 'wb') as f:
+            pickle.dump(self.schedule, f)
+    @staticmethod
+    def save_schedule_as_xls(byte: bytes, name: str = 'schedule.xls') -> None:
+        path = pathlib.Path(name).with_suffix('.xls')
+        with open(path, 'wb') as f:
+            f.write(byte)
+
+    @staticmethod
+    def LessonDecoder(d: dict) -> Any:
+        if 'name' in d:
+            return Lesson(**d)
+        else:
+            return d
+
 
 class Lesson:
-    def __init__(self, name, teacher, time, location):
+    def __init__(self, name='', teacher: Union[str, list] = [], time: Union[str, list] = [], location: str = ''):
         self.name: str = name
         self.time = self._fmt_time(time)
         self.location = self._fmt_location(location)
@@ -67,88 +144,12 @@ class Lesson:
     def _fmt_location(s: str) -> str:
         return s.replace('Ｎ', 'N')
 
-
-class LessonEncoder(json.JSONEncoder):
-    def default(self, o):
-        return o.__dict__
-
-
-def load_schedule(byte: Union[bytes, io.IOBase]) -> dict:
-    # 如果byte是open的文件对象
-    if isinstance(byte, io.IOBase):
-        byte = byte.read()
-    # 如果byte是bytes对象
-    elif isinstance(byte, bytes):
-        byte = io.BytesIO(byte)
-
-    excel = pandas.read_excel(byte, sheet_name='Sheet1')
-    lesson_dict = {}
-    for column in excel.columns[1:]:
-        # 遍历表格每一行
-        day = excel[column][1]
-        lesson_dict[day] = {}
-        for index, row in excel[column].items():
-            if 1 < index < 7:
-                lesson_list = set()
-                classes_list = row.split('\n\n')
-                for classes in classes_list:
-                    if classes == ' ':
-                        lesson = Lesson()
-                    else:
-                        info = [i for i in classes.split('\n') if i != '']
-                        lesson = Lesson(*info)
-                    lesson_list.add(lesson)
-                lesson_dict[day][index - 2] = list(lesson_list)
-    return lesson_dict
-
-
-def save_schedule_file(byte: bytes, name: str = 'schedule.xls') -> None:
-    path = pathlib.Path(name).with_suffix('.xls')
-    with open(path, 'wb') as f:
-        f.write(byte)
-
-
-def save_schedule_as_json(schedule: dict, name: str = 'schedule.json') -> None:
-    path = pathlib.Path(name)
-    with open(path, 'w') as f:
-        json.dump(schedule, f, indent=4, ensure_ascii=False, cls=LessonEncoder)
-
-
-def save_schedule_as_pickle(schedule: dict, name: str = 'schedule.pickle') -> None:
-    path = pathlib.Path(name)
-    with open(path, 'wb') as f:
-        pickle.dump(schedule, f)
-
-
-def LessonDecoder(d: dict) -> Any:
-    if 'name' in d:
-        return Lesson(**d)
-    else:
-        return d
-
-
-def load_schedule_from_json(name: str = 'schedule.json') -> dict:
-    path = pathlib.Path(name)
-    with open(path, 'r') as f:
-        return json.load(f, object_hook=LessonDecoder)
-
-
-def load_schedule_from_pickle(name: str = 'schedule.pickle') -> dict:
-    path = pathlib.Path(name)
-    with open(path, 'rb') as f:
-        return pickle.load(f)
+    class LessonEncoder(json.JSONEncoder):
+        def default(self, o):
+            return o.__dict__
 
 
 if __name__ == '__main__':
-    # rq = Request()
-    # rq.login()
-    # schedule_table = rq.get_schedule('2022-2023-2')
-    # save_schedule_file(schedule_table)
-    # with open('schedule.xls', 'rb') as f:
-    #     schedule_table = f.read()
-    # lessons = load_schedule(schedule_table)
-    # save_schedule_as_json(lessons)
-    # save_schedule_as_pickle(lessons)
-    # lessons = load_schedule_from_json()
-    # lessons = load_schedule_from_pickle()
+    s = Schedule('schedule.xls')
+    s.save_schedule_as_json()
     pass
