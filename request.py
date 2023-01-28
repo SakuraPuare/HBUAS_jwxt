7f9f0a3c91ff91d6f89a18724e0144a52f1f512b
SakuraPuare <java20131114@gmail.com>
2023-01-28 16:44:17 +0800

    ✨feat: post 学生成绩

--- request.py
+++ request.py
@@ -80,7 +80,28 @@ class Request:
         return self.session.post(self.base_url + 'xskb/xskb_print.do',
                                  headers=self.headers, data={'xnxq01id': semester, 'zc': ''})
 
-    
+    def post_lesson_score(self, semester: str, types: str = '', score: str = 'all') -> httpx.Response:
+        select = {
+            '': '',
+            '公共课': '01',
+            '公共基础课': '02',
+            '专业基础课': '03',
+            '专业课': '04',
+            '专业选修课': '05',
+            '公共选修课': '06',
+            '通识教育必修': '07',
+            '学科基础课': '08',
+            '专业核心课': '09',
+            '方向必修课': '10',
+            '方向选修课': '11',
+            '通识教育选修': '12',
+            '其他': '13',
+            '专业方向课': '14',
+            '专业必修课': '15',
+            '创新创业课': '16',
+        }
+        data = {'kksj': semester, 'kcxz': select.get(types), 'kcmc': '', 'xsfs': score}
+        return self.session.post(self.base_url + 'kscj/cjcx_list', headers=self.headers, data=data)
 
 
 if __name__ == "__main__":
